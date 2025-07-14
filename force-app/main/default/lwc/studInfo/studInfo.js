b9153cd6739d72638f6d733f1be5bb0f2e48b653
Praju-Phapale-Thete <prajaktathete8503@gmail.com>
2025-07-14 11:54:08 +0530

    Update studInfo.js

--- force-app/main/default/lwc/studInfo/studInfo.js
+++ force-app/main/default/lwc/studInfo/studInfo.js
@@ -4,6 +4,7 @@ export default class StudInfo extends LightningElement {
     @api studInfo;
 
     tileclickHandler() {
+        alert('tileclickHandler was called');
         if (this.studInfo) {
             const selectEvent = new CustomEvent('tileclick', {
                 detail: this.studInfo
